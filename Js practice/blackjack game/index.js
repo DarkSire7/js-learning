08acf07ace2a2037a08f26059eda33db73f11a68
darksire7 <akifullahahmed1@gmail.com>
2025-05-01 01:34:34 +0530

    BlackJack game progress

--- Js practice/blackjack game/index.js
+++ Js practice/blackjack game/index.js
@@ -1,28 +1,51 @@
 let sum = 0;
 
+let message = document.getElementById("message");
+
+let cardhtml = document.getElementById("card");
+let sumhtml = document.getElementById("sum");
+
+let newbutton = document.getElementById("new");
+
+newbutton.style.opacity = 0;
+
 function bj() {
     let firstCard = Math.ceil(Math.random() * 10) + 1;
     sum += firstCard;
+    cardhtml.innerHTML += " " + firstCard;
+    sumhtml.innerHTML = "Sum: " + sum;
 }
 
 
 function check() {
     if (sum < 21) {
-        console.log("Pick another card? D:")
-        bj();
-        console.log(sum);
-        check();
+        message.innerHTML = "Pick another card?";
     }
     else if (sum === 21) {
-        console.log("YOU WON :D")
+        message.innerHTML = "YOU WON";
+        hasBlackJack = true;
     }
     else {
-        console.log("You Lost :(")
+        message.innerHTML = "You Lost";
+        isAlive = false;
     }
 }
 
-bj();
-bj();
 
-console.log(sum);
-check();
+
+
+function start() {
+    if (sum === 0) {
+        bj();
+        bj();
+        newbutton.style.opacity = 100;
+        check();
+    }
+}
+
+function newcard() {
+
+    bj();
+    check();
+
+}
