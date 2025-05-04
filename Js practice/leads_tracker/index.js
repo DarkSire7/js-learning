6b25e4d4866c5b52f09c04ffa8175d6c02f47cf6
darksire7 <akifullahahmed1@gmail.com>
2025-05-04 14:41:32 +0530

    Added Save functionality and an Icon

--- Js practice/leads_tracker/index.js
+++ Js practice/leads_tracker/index.js
@@ -1,26 +1,46 @@
 const savebtn = document.getElementById("save-btn");
+const clearbtn = document.getElementById("clear-btn");
+
+
 const inputhtml = document.getElementById("input-field");
 let listhtml = document.getElementById("list");
 
-
-
 let myleads = []
 
+if (localStorage.length != 0) {
+    myleads = JSON.parse(localStorage.myLeads)
+}
+
+renderleads(myleads);
+
 savebtn.addEventListener("click", function save() {
+    console.log(1)
     myleads.push(inputhtml.value)
-    renderleads();
+    localStorage.setItem("myLeads", JSON.stringify(myleads));
+
+    renderleads(myleads);
     clear();
 })
 
+clearbtn.addEventListener("dblclick", function clear() {
+    localStorage.clear();
+    listhtml.innerHTML = "";
+    myleads = [];
 
-function renderleads() {
-    let listitems = "";
+})
 
-    for (let i = 0; i < myleads.length; i++) {
+
+
+function renderleads(leads) {
+    listitems = "";
+
+
+
+    for (let i = 0; i < leads.length; i++) {
         listitems += `
         <li>
-            <a href = '${myleads[i]}' target='_blank'> 
-            ${myleads[i]} 
+            <a href = '${leads[i]}' target='_blank'> 
+            ${leads[i]} 
             </a>
         </li>`
     }
